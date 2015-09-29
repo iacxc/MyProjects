15cad07b3e68411c85c6e8275f36186a258b9fc5
Chengxin.Cai <chengxin.cai@gmail.com>
2015-09-29 21:33:19 +0800

    new files

---
+++ Scala/selftype.scala
@@ -0,0 +1,39 @@
+
+trait Persistence {
+    def startPersistence: Unit
+}
+
+trait Midtier {
+    def startMidtier: Unit
+}
+
+trait UI {
+    def startUI: Unit
+}
+
+trait Database extends Persistence {
+    def startPersistence = println("Starting Database")
+}
+
+trait ComputeCluster extends Midtier {
+    def startMidtier = println("Starting ComputeCluster")
+}
+
+trait WebUI extends UI {
+    def startUI = println("Starting WebUI")
+}
+
+trait App {
+    self: Persistence with Midtier with UI =>
+
+    def run = {
+        startPersistence
+        startMidtier
+        startUI
+    }
+}
+
+object MyApp extends App with Database with ComputeCluster with WebUI
+
+MyApp.run
+
