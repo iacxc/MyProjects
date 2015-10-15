b36909a552f442f6c70784067e1d8798d6e4b6a8
Chengxin.Cai <chengxin.cai@gmail.com>
2015-10-15 21:01:30 +0800

    add new function

--- Utils/list.py
+++ Utils/list.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python
 
-class Cons(object):
-    def __init__(self, head, tail):
-        self.__head = head
-        self.__tail = tail
 
-    @property
-    def head(self):
-        return self.__head
-
-    @property
-    def tail(self):
-        return self.__tail
-
-    @property
-    def isEmpty(self):
-        return False
-
-    def __str__(self):
-        return "({0} {1})".format(self.head, self.tail)
+__all__ = ('List',)
 
 
 class Nil(object):
@@ -38,6 +21,27 @@ class Nil(object):
         return "()"
 
 
+class Cons(object):
+    def __init__(self, head, tail=Nil):
+        self.__head = head
+        self.__tail = tail
+
+    @property
+    def head(self):
+        return self.__head
+
+    @property
+    def tail(self):
+        return self.__tail
+
+    @property
+    def isEmpty(self):
+        return False
+
+    def __str__(self):
+        return "({0} {1})".format(self.head, self.tail)
+
+
 def List(*items):
     if len(items) == 0:
         return Nil()
@@ -45,7 +49,23 @@ def List(*items):
         return Cons(items[0], List(*items[1:]))
 
 
+def reverse(alist):
+    def rec(lst, acc):
+        if lst.isEmpty:
+            return acc
+        else:
+            return rec(lst.tail, Cons(lst.head, acc))
+
+    return rec(alist, Nil())
+
+#    if list.isEmpty or list.tail.isEmpty:
+#        return list
+#
+#    return Cons(reverse(list.tail), List(list.head))
+
+
 l = List(1, 2, List("hello", "world"), "a", "b", List(List('aa', 'bb')))
 print l
-print l.head, type(l.head)
-print l.tail, type(l.tail)
+l2 = List(List(List('aa', 'bb')), "b", "a", List("hello", "world"), 2, 1) 
+print l2
+print reverse(l)
