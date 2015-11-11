31847639b9d733e45465a87ab12cc04479b80c73
Cai Chengxin <chengxin.cai@gmail.com>
2015-11-11 19:55:45 +0800

    add more operations

--- Hadoop/hadoop.scala
+++ Hadoop/hadoop.scala
@@ -4,8 +4,22 @@ trait HadoopUtil {
     val host: String
     val port: Int
 
-    def baseurl = s"$prefix://$host:$port"
+    def baseurl = "%s://%s:%s".format(prefix, host, port)
 
+}
+
+object Curl{
+    private def curl(opt: String) = 
+        "curl -L" + (if (opt.length > 0) " " + opt else "")
+
+    def Get(url: String, curlopt: String="") = 
+        curl(curlopt) + " '" + url + "'"
+    def Put(url: String, curlopt: String="") = 
+        curl(curlopt) + " -X PUT '" + url + "'"
+    def Delete(url: String, curlopt: String="") = 
+        curl(curlopt) + " -X DELETE '" + url + "'"
+    def Post(url: String, curlopt: String="") = 
+        curl(curlopt) + " -X POST '" + url + "'"
 }
 
 class Hdfs(h: String, user: String) extends HadoopUtil {
@@ -13,29 +27,50 @@ class Hdfs(h: String, user: String) extends HadoopUtil {
     val port = 50070
 
     lazy val userspec = "&user.name=" + user
+    lazy val weburl = baseurl + "/webhdfs/v1"
 
+    import Curl._
     def ls(dirname: String) = 
-        baseurl + s"/webhdfs/v1$dirname?op=LISTSTATUS$userspec"
+        Get(weburl + dirname + "?op=LISTSTATUS" + userspec)
 
-    def mkdir(dirname: String, perm: String = "777") = 
-        baseurl + s"/webhdfs/v1$dirname?op=MKDIRS&permission=$perm$userspec"
+    def mkdir(dirname: String, perm: String="777") = 
+        Put(weburl + dirname +
+            "?op=MKDIRS&permission=" + perm + userspec)
 
     def create(filename: String) = 
-        baseurl + s"/webhdfs/v1$filename?op=CREATE$userspec"
+        Put(weburl + filename +"?op=CREATE" + userspec)
 
     def cp(localfile: String, filename: String) =
-        baseurl + s"/webhdfs/v1$filename?op=CREATE$userspec"
+        Put(weburl + filename + "?op=CREATE" + userspec,
+            "-T " + localfile)
+
+    def append(localfile: String, filename: String) =
+        Put(weburl + filename + "?op=APPEND" + userspec,
+            "-T " + localfile)
 
     def delete(filename: String) = 
-        baseurl + s"/webhdfs/v1$filename?op=DELETE$userspec"
+        Delete(weburl + filename + "?op=DELETE" + userspec)
+
+    def stat(filename: String) = 
+        Get(weburl + filename + "?op=GETFILESTATUS" + userspec)
 
     def cat(filename: String) = 
-        baseurl + s"/webhdfs/v1$filename?op=OPEN$userspec"
+        Get(weburl + filename + "?op=OPEN" + userspec)
 
     def rename(srcname: String, destname: String) =
-        baseurl + "/webhdfs/v1" + srcname + 
-                  "?op=RENAME&destination=" + destname + 
-                  userspec
+        Put(weburl + "/webhdfs/v1" + srcname + 
+            "?op=RENAME&destination=" + destname + userspec)
+
+    def chmod(filename: String, perm: String="700") =
+        Put(weburl + filename + 
+            "?op=SETPERMISSION&permission=" + perm + userspec)
+
+    def chown(filename: String, owner: String, group: String="") = 
+        Put(weburl + filename +
+            "?op=SETOWNER&owner=" + owner +
+            (if (group.length > 0) "&group=" + group else "") +
+            userspec)
+
 }
 
 object Hdfs {
@@ -66,8 +101,14 @@ object Knox {
 */
 
 object HadoopTest extends App {
-    val hdfs = Hdfs("adl03", "caiche")
+    val host = if (args.length > 0) args(0) else "localhost"
+    val hdfs = Hdfs(host, "caiche")
     println(hdfs.ls("/user/caiche"))
     println(hdfs.mkdir("/user/caiche/test", "700"))
-    println(hdfs.cat("/user/caiche/test.txt"))
+    println(hdfs.cp("hadoop.scala", "/user/caiche/test.1"))
+    println(hdfs.append("kdfs.cmd1", "/user/caiche/test.2"))
+    println(hdfs.chown("/user/caiche/test.3", "caiche", "caiche"))
+    println(hdfs.chmod("/user/caiche/test.4", "700"))
+    println(hdfs.stat("/user/caiche/test.2"))
+    println(hdfs.cat("/user/caiche/test.2"))
 }
