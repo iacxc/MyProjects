6abb09f1a862c8678b00f379459b01497a88a286
Chengxin.Cai <chengxin.cai@gmail.com>
2015-11-26 13:16:33 +0800

    add more

--- Hadoop/HadoopUtil.py
+++ Hadoop/HadoopUtil.py
@@ -32,7 +32,8 @@ def get_opstr(operation):
             "rename": "RENAME"}[operation]
 
 
-def Request(method, url, user=None, auth=None, params=None, data=None):
+def Request(method, url, user=None, auth=None, params=None, 
+                         data=None, headers=None):
     if params is None:
         params = {}
 
@@ -42,8 +43,7 @@ def Request(method, url, user=None, auth=None, params=None, data=None):
     if __debug__: print method, url, auth, params
 
     return requests.request(method, url, auth=auth, verify=False, 
-                                         params=params, data=data)
-
+                            params=params, data=data, headers=headers)
 
 
 class HadoopUtil(object):
@@ -52,9 +52,11 @@ class HadoopUtil(object):
         self.__host = host
         self.__port = port
 
+
     @property
     def baseurl(self):
         return "%s://%s:%s" % (self.__prefix, self.__host, self.__port)
+
 
     @staticmethod
     def gen_filespec(fs):
@@ -73,21 +75,25 @@ class Hdfs(HadoopUtil):
         super(Hdfs, self).__init__("http", host, 50070)
         self.__user = user
 
+
     @staticmethod
     def Get(url, operation, user=None, auth=None, params={}):
         params["op"] = get_opstr(operation)
 
         return Request("GET", url, user, auth, params)
 
+
     @staticmethod
     def Delete(url, user=None, auth=None, params=None):
         params["op"] = "delete"
         return Request("DELETE", url, user, auth, params=params)
 
+
     @staticmethod
     def Put(url, operation, user=None, auth=None, params={}, data=None):
         params["op"] = get_opstr(operation)
         return Request("PUT", url, user, auth, params, data)
+
 
     @staticmethod
     def Post(url, operation, user=None, auth=None, params=None, data=None):
@@ -107,7 +113,6 @@ class Hdfs(HadoopUtil):
             return [self.gen_filespec(fs) for fs in fs_list]
         else:
             if __debug__: print resp.status_code
-            return {}
 
 
     def mkdir(self, dirname, perm="777") : 
@@ -117,7 +122,6 @@ class Hdfs(HadoopUtil):
             return resp.json()["boolean"]
         else:
             if __debug__: print resp.status_code
-            return False
 
 
     def cp(self, localfile, filename) :
@@ -126,10 +130,9 @@ class Hdfs(HadoopUtil):
                        params={"overwrite" : "true"}, data=f.read())
 
             if resp.status_code in (requests.codes.ok, requests.codes.created):
-                return True
+                return resp.json()
             else:
                 if __debug__: print resp.status_code
-                return False
 
 
     def append(self, localfile, filename) :
@@ -138,19 +141,17 @@ class Hdfs(HadoopUtil):
                         data=f.read())
 
             if resp.status_code == requests.codes.ok:
-                return True
+                return resp.json()
             else:
                 if __debug__: print resp.status_code, resp.text
-                return False
 
 
     def delete(self, filename) : 
         resp = Hdfs.Delete(self.weburl + filename, self.__user)
         if resp.status_code == requests.codes.ok:
-            return resp.json()["boolean"]
+            return resp.json()
         else:
             if __debug__: print resp.status_code
-            return False
 
 
     def stat(self, filename):
@@ -159,7 +160,6 @@ class Hdfs(HadoopUtil):
             return self.gen_filespec(resp.json()["FileStatus"])
         else:
             if __debug__: print resp.status_code
-            return {}
 
 
     def cat(self, filename):
@@ -168,27 +168,24 @@ class Hdfs(HadoopUtil):
             return resp.text
         else:
             if __debug__: print resp.status_code
-            return ""
 
 
     def rename(self, srcname, destname):
         resp = Hdfs.Put(self.weburl + srcname, "rename", self.__user,
                    params={"destination" : destname})
         if resp.status_code == requests.codes.ok:
-            return resp.json()["boolean"]
+            return resp.json()
         else:
             if __debug__: print resp.status_code
-            return False
 
 
     def chmod(self, filename, perm="777"):
         resp = Hdfs.Put(self.weburl + filename, "chmod", self.__user,
                    params={"permission" : perm})
         if resp.status_code == requests.codes.ok:
-            return True
+            return resp.json()
         else:
             if __debug__: print resp.status_code
-            return False
 
 
     def chown(self, filename, owner, group=None):
@@ -196,10 +193,9 @@ class Hdfs(HadoopUtil):
                    params={"owner" : owner,
                            "group" : group})
         if resp.status_code == requests.codes.ok:
-            return True
+            return resp.json()
         else:
             if __debug__: print resp.status_code
-            return False
 
 
 class HCat(HadoopUtil):
@@ -210,6 +206,74 @@ class HCat(HadoopUtil):
     @staticmethod
     def Get(url, user=None, auth=None, params={}):
         return Request("GET", url, user, auth, params)
+
+
+class Ranger(HadoopUtil):
+    def __init__(self, host, user, password):
+        super(Ranger, self).__init__("http", host, 6080)
+        self.__user = user
+        self.__password = password
+
+        self.weburl = self.baseurl + "/service/public/api"
+        self.repourl = self.weburl + "/repository"
+        self.policyurl = self.weburl + "/policy"
+
+    @property
+    def auth(self):
+        return (self.__user, self.__password)
+
+    @staticmethod
+    def Get(url, auth):
+        return Request("GET", url, auth=auth)
+
+    @staticmethod
+    def Post(url, auth, data):
+        return Request("POST", url, auth=auth, 
+                       data=data, 
+                       headers={"Content-Type" : "Application/json"})
+
+
+    def get_repository(self, id=None):
+        url = self.repourl if id is None else "%s/%d" % (self.repourl, id)
+
+        resp = Ranger.Get(url, auth=self.auth)
+        if resp.status_code == requests.codes.ok:
+            return resp.json()
+        else:
+            if __debug__: print resp.status_code
+
+
+    def get_policy(self, id=None):
+        url = self.policyurl if id is None else "%s/%d" % (self.policyurl, id)
+
+        resp = Ranger.Get(url, auth=self.auth)
+        if resp.status_code == requests.codes.ok:
+            return resp.json()
+        else:
+            if __debug__: print resp.status_code
+
+    def create_hdfs_policy(self, repo_name, policy_name, filename, 
+                           perm_map_list,
+                           description="", is_enabled=True, 
+                           is_recursive=True, is_audit_enabled=True):
+        datadict = {
+            "policyName"     : policy_name,
+            "resourceName"   : filename,
+            "description"    : description,
+            "repositoryName" : repo_name,
+            "repositoryType" : "hdfs",
+            "isEnabled"      : is_enabled,
+            "isRecursive"    : is_recursive,
+            "isAuditEnabled" : is_audit_enabled,
+            "permMapList"    : perm_map_list
+        }
+        resp = Ranger.Post(self.policyurl, 
+                           auth=self.auth, 
+                           data=json.dumps(datadict))
+        if resp.status_code == requests.codes.ok:
+            return resp.json()
+        else:
+            if __debug__: print resp.status_code, resp.text
 
 
 class Knox(HadoopUtil):
@@ -234,7 +298,6 @@ class Knox(HadoopUtil):
             return [self.gen_filespec(fs) for fs in fs_list]
         else:
             if __debug__: print resp.status_code
-            return {}
 
     def cat(self, filename):
         resp = Hdfs.Get(self.hdfsurl + filename, "cat", auth=self.auth)
@@ -279,8 +342,8 @@ if __name__ == "__main__":
     host = sys.argv[1] if len(sys.argv) > 1 else "localhost"
     user = sys.argv[2] if len(sys.argv) > 2 else "caiche"
     user_pass = sys.argv[3] if len(sys.argv) > 3 else "caiche-password"
-    hdfs = Hdfs(host, user)
-    print "\n".join(hdfs.ls("/user/caiche"))
+    #hdfs = Hdfs(host, user)
+    #print "\n".join(hdfs.ls("/user/caiche"))
     #print hdfs.mkdir("/user/caiche/test3")
     #print hdfs.stat("/user/caiche/test.1")
     #print hdfs.delete("/user/caiche/test.3")
@@ -288,11 +351,28 @@ if __name__ == "__main__":
     #print hdfs.append("run.cmd1", "/user/caiche/test.x")
     #print "\n".join(hdfs.ls("/user/caiche"))
 
-    knox = Knox(host, user, user_pass)
-    print "\n".join(knox.ls("/user/caiche"))
-    print json.dumps(knox.get_database(), indent=4)
-    print json.dumps(knox.get_database("default"), indent=4)
-    print json.dumps(knox.get_table("default"), indent=4)
-    print json.dumps(knox.get_table("default", "u_data"), indent=4)
-
+    #knox = Knox(host, user, user_pass)
+    #print "\n".join(knox.ls("/user/caiche"))
     #print knox.cat("/user/caiche/test.1")
+    #print json.dumps(knox.get_database(), indent=4)
+    #print json.dumps(knox.get_database("default"), indent=4)
+    #print json.dumps(knox.get_table("default"), indent=4)
+    #print json.dumps(knox.get_table("default", "u_data"), indent=4)
+
+    ranger = Ranger(host, user, user_pass)
+    print json.dumps(ranger.get_repository(), indent=4)
+    #print json.dumps(ranger.get_repository(6), indent=4)
+    #print json.dumps(ranger.get_policy(), indent=4)
+    #print json.dumps(ranger.get_policy(13), indent=4)
+    #print ranger.create_hdfs_policy("hdp230_2_hadoop", 
+    #                                "hdfs-test-3", 
+    #                                "/user/caiche",
+    #                                [{"userList" : ["caiche"],
+    #                                  "groupList" : [],
+    #                                  "permList" : ["Read", "Write", "Execute"]
+    #                                 },
+    #                                 {"userList" : ["guest"],
+    #                                  "groupList" : [],
+    #                                  "permList" : ["Read", "Execute"]
+    #                                 }])
+
