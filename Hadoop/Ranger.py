7eaa4224f5b1220c7d003d673acc19605e001e85
Chengxin.Cai <chengxin.cai@gmail.com>
2015-12-06 22:03:00 +0800

    add new files

--- Hadoop/Ranger.py
+++ Hadoop/Ranger.py
@@ -3,31 +3,33 @@
 #exports
 __all__ = ('Ranger',)
 
-from HadoopUtil import HadoopUtil, Request, \
+import json
+from HadoopUtil import HadoopUtil, Request, CmdTuple, \
                        STATUS_NOCONTENT 
 
 class Ranger(HadoopUtil):
-    operations = ("get_repository",
-                  "get_policy", "delete_policy", "create_policy",)
     rootpath = "/service/public/api"
+    commands = HadoopUtil.commands + [
+        "",
+        CmdTuple("list repository",    "",             "List all repositories"),
+        CmdTuple("list policy",        "",             "List all policies"),
+        CmdTuple("show repository",    "<id>",         "Show a repository"),
+        CmdTuple("show policy",        "<id>",         "Show a policy"),
+        CmdTuple("create repository", "<data>",        "Create a repository"),
+        CmdTuple("create policy",     "<data>",        "Create a policy"),
+        CmdTuple("update repository", "<id> <data>",   "Update a repository"),
+        CmdTuple("update policy",     "<id> <data>",   "Update a policy"),
+        CmdTuple("delete repository", "<id>",          "Delete a repository"),
+        CmdTuple("delete policy",     "<id>",          "Delete a policy"),
+        ]
 
-    def __init__(self, host, user, password, curl=False):
-        super(Ranger, self).__init__("http", host, 6080)
-        self.__user = user
-        self.__password = password
-        self.__curl = curl
+    def __init__(self, host, user, passwd):
+        HadoopUtil.__init__(self, "http", host, 6080, user, passwd)
 
         self.weburl = self.baseurl + self.rootpath
         self.repourl = self.weburl + "/repository"
         self.policyurl = self.weburl + "/policy"
 
-    def __iter__(self):
-        return iter(self.operations)
-
-
-    @property
-    def auth(self):
-        return (self.__user, self.__password)
 
     @staticmethod
     def Get(url, auth, params=None, curl=False):
@@ -49,31 +51,133 @@ class Ranger(HadoopUtil):
                        data=data, 
                        headers={"Content-Type" : "Application/json"}, curl=curl)
 
+    @property
+    def banner(self):
+        return "Ranger Shell"
+
+
+    def do_list(self, data):
+        if data == 'repository':
+            self.do_echo(self.get_repository())
+        elif data == 'policy':
+            self.do_echo(self.get_policy())
+        else:
+            print "Invalid parameter '%s'" % data
+
+
+    def do_show(self, data):
+        params = data.split()
+        if len(params) < 2:
+            print "Not enough parameters"
+            return 
+
+        if params[0] == 'repository':
+            self.do_echo(self.get_repository(params[1]))
+        elif params[0] == 'policy':
+            self.do_echo(self.get_policy(params[1]))
+        else:
+            print "Invalid parameter '%s'" % params[0]
+
+
+    def do_create(self, data):
+        params = data.split()
+        if len(params) < 2:
+            print "Not enough parameters"
+            return 
+
+        try:
+            postdata = " ".join(params[1:])
+            if params[0] == 'repository':
+                print "Create repository", json.loads(postdata)
+            elif params[0] == 'policy':
+                print "Create policy", json.loads(postdata)
+            else:
+                print "Invalid parameter '%s'" % params[0]
+
+        except TypeError as e:
+             print e
+        except ValueError as e:
+             print e
+
+
+    def do_update(self, data):
+        params = data.split()
+        if len(params) < 3:
+            print "Not enough parameters"
+            return 
+
+        try:
+            postdata = " ".join(params[2:])
+            if params[0] == 'repository':
+                print "Update repository " + params[1], json.loads(postdata)
+            elif params[0] == 'policy':
+                print "Update policy " + params[1], json.loads(postdata)
+            else:
+                print "Invalid parameter '%s'" % params[0]
+        
+        except TypeError as e:
+             print e
+        except ValueError as e:
+             print e
+
+
+    def do_delete(self, data):
+        params = data.split()
+        if len(params) < 2:
+            print "Not enough parameters"
+
+        if params[0] == 'repository':
+            self.do_echo(self.delete_repository(params[1]))
+        elif params[0] == 'policy':
+            self.do_echo(self.delete_policy(params[1]))
+        else:
+            print "Invalid parameter '%s'" % params[0]
+
 
     def get_repository(self, service_id=None):
         url = self.repourl if service_id is None \
                            else "%s/%s" % (self.repourl, service_id)
 
-        return Ranger.Get(url, auth=self.auth, curl=self.__curl)
+        return Ranger.Get(url, auth=self.auth, curl=self.curl)
 
 
     def get_policy(self, policy_id=None, params=None):
         url = self.policyurl if policy_id in (None, "None") \
                              else "%s/%s" % (self.policyurl, policy_id)
 
-        return Ranger.Get(url, auth=self.auth, params=params, curl=self.__curl)
+        return Ranger.Get(url, auth=self.auth, params=params, curl=self.curl)
+
+
+    def delete_repository(self, repo_id):
+        r = Ranger.Delete("%s/%s" % (self.repourl, repo_id), 
+                          auth=self.auth, 
+                          curl=self.curl, expected=(STATUS_NOCONTENT,))
+        if r is not None:
+            return {"status": "ok"}
 
 
     def delete_policy(self, policy_id):
         r = Ranger.Delete("%s/%s" % (self.policyurl, policy_id), 
                           auth=self.auth, 
-                          curl=self.__curl, expected=(STATUS_NOCONTENT,))
+                          curl=self.curl, expected=(STATUS_NOCONTENT,))
         if r is not None:
             return {"status": "ok"}
 
 
     def create_policy(self, service_type, service_name, policy_name, 
                             policy_data):
+        if isinstance(policy_data, str):
+            policy_data = json.loads(policy_data)
+
+        policy_data.update(policyName=policy_name,
+                           repositoryName=service_name)
+    
+        return Ranger.Post(self.policyurl, 
+                           auth=self.auth, 
+                           data=json.dumps(policy_data),
+                           curl=self.curl)
+
+
         dispatcher = getattr(self, "create_%s_policy" % service_type)
         return dispatcher(service_name, policy_name, policy_data)
 
@@ -88,58 +192,42 @@ class Ranger(HadoopUtil):
         return Ranger.Put("%s/%s" % (self.policyurl, policy_id), 
                            auth=self.auth, 
                            data=json.dumps(policy_data), 
-                           curl=self.__curl)
-
-
-    def create_hdfs_policy(self, service_name, policy_name, policy_data):
-        if isinstance(policy_data, str):
-            policy_data = json.loads(policy_data)
-
-        policy_data.update(policyName=policy_name,
-                           repositoryName=service_name,
-                           repositoryType="hdfs")
-        policy_data.setdefault("isEnabled"     , True)
-        policy_data.setdefault("isRecursive"   , True)
-        policy_data.setdefault("isAuditEnabled", True)
-    
-        return Ranger.Post(self.policyurl, 
-                           auth=self.auth, 
-                           data=json.dumps(policy_data),
-                           curl=self.__curl)
+                           curl=self.curl)
 
 
 #
 # ---- main ----
 if __name__ == "__main__":
-    import sys
-    import json
-    from optparse import OptionParser
-
-    parser = OptionParser()
-    parser.add_option("--host", default="localhost")
-    parser.add_option("--curl", action="store_true", default=False)
-    parser.add_option("-u", "--user", default="caiche")
-    parser.add_option("-p", "--password", default="caiche-password")
-
-    opts, args = parser.parse_args()
-
-    if __debug__: print opts, args
-
-    if len(args) < 1:
-        print "Missing arguments, supported methods are", Ranger.operations
-        sys.exit(1)
-
-    ranger = Ranger(opts.host, opts.user, opts.password, opts.curl)
-
-    method = args[0]
-    if not method in ranger:
-        print "Unsupported method '%s'" % method
-        sys.exit(1)
-
-    try:
-        fun = getattr(ranger, method)
-        print json.dumps(fun(*args[1:]), indent=4)
-
-    except AttributeError as e:
-        print e
-
+    print "Ranger"
+#    import sys
+#    import json
+#    from optparse import OptionParser
+#
+#    parser = OptionParser()
+#    parser.add_option("--host", default="localhost")
+#    parser.add_option("--curl", action="store_true", default=False)
+#    parser.add_option("-u", "--user", default="caiche")
+#    parser.add_option("-p", "--password", default="caiche-password")
+#
+#    opts, args = parser.parse_args()
+#
+#    if __debug__: print opts, args
+#
+#    if len(args) < 1:
+#        print "Missing arguments, supported methods are", Ranger.operations
+#        sys.exit(1)
+#
+#    ranger = Ranger(opts.host, opts.user, opts.password, opts.curl)
+#
+#    method = args[0]
+#    if not method in ranger:
+#        print "Unsupported method '%s'" % method
+#        sys.exit(1)
+#
+#    try:
+#        fun = getattr(ranger, method)
+#        print json.dumps(fun(*args[1:]), indent=4)
+#
+#    except AttributeError as e:
+#        print e
+#
--- Hadoop/WebHdfs.py
+++ Hadoop/WebHdfs.py
@@ -4,13 +4,13 @@
 __all__ = ('WebHdfs',)
 
 
-from HadoopUtil import HadoopUtil, Request, \
+from HadoopUtil import HadoopUtil, Request, CmdTuple, \
                        gen_fileinfo, \
                        STATUS_OK, STATUS_CREATED
+import os
 
 def get_opstr(op):
     return {"ls"    : "LISTSTATUS",
-            "stat"  : "GETFILESTATUS",
             "cat"   : "OPEN",
             "mkdir" : "MKDIRS",
             "cp"    : "CREATE",
@@ -22,23 +22,37 @@ def get_opstr(op):
 
 
 class WebHdfs(HadoopUtil):
-    operations = ("ls", "mkdir", "cp", "append", "delete",
-                  "stat", "cat", "rename", "chmod", "chown", )
     rootpath = "/webhdfs/v1"
+    commands = HadoopUtil.commands + [
+        "",
+        CmdTuple("lls",    "",  "List local files/directoies"),
+        CmdTuple("ls",     "",  "List files/directoies"),
+        CmdTuple("pwd",    "",  "Show the current dir"),
+        CmdTuple("cd",     "",  "Change the current dir"),
+        CmdTuple("cat",    "",  "Type a text file"),
+        CmdTuple("mkdir",  "",  "Show a policy"),
+        CmdTuple("cp",     "",  "Create a repository"),
+        CmdTuple("append", "",  "Create a policy"),
+        CmdTuple("chmod",  "",  "Update a repository"),
+        CmdTuple("chown",  "",  "Update a policy"),
+        CmdTuple("delete", "",  "Delete a repository"),
+        CmdTuple("rename", "",  "Delete a policy"),
+        ]
 
-    def __init__(self, host, user, curl=False):
-        super(WebHdfs, self).__init__("http", host, 50070)
-        self.__user = user
-        self.__curl = curl
 
-
-    def __iter__(self):
-        return iter(self.operations)
+    def __init__(self, host, user):
+        HadoopUtil.__init__(self, "http", host, 50070, user)
+        self.__curdir = self.home
 
 
     @property
     def weburl(self):
         return self.baseurl + self.rootpath
+
+
+    @property
+    def home(self):
+        return "/user/%s" % self.user
 
 
     @staticmethod
@@ -76,119 +90,203 @@ class WebHdfs(HadoopUtil):
 
 
 #-- operations
-    def ls(self, dirname):
-        result = self.Get(self.weburl + dirname, "ls", self.__user, 
-                          curl=self.__curl)
+    def do_lls(self, data):
+        os.system("ls %s" % data)
+
+
+    def do_pwd(self, data):
+        print self.__curdir
+
+
+    def do_cd(self, data):
+        if data:
+            if data == "..":
+                self.__curdir = os.path.dirname(self.__curdir)
+            elif data[0] == "/":
+                self.__curdir = data
+            else:
+                self.__curdir = "%s/%s" % (self.__curdir, data)
+        else:
+            self.__curdir = self.home
+
+
+    def do_ls(self, filename):
+        if len(filename) == 0:
+            filename = self.__curdir
+
+        result = self.Get(self.weburl + filename, "ls", self.user, 
+                          curl=self.curl)
 
         if result is not None:
-            fs_list = result["FileStatuses"]["FileStatus"]
-            return [gen_fileinfo(fs) for fs in fs_list]
+            if result.get("FileStatuses"):
+                fs_list = result["FileStatuses"]["FileStatus"]
+                print "\n".join(gen_fileinfo(fs) for fs in fs_list)
+            else:
+                print "File not found"
 
 
-    def mkdir(self, dirname, perm="777") : 
-        return self.Put(self.weburl + dirname, "mkdir", self.__user, 
+    def do_mkdir(self, data):
+        params = data.split()
+        if len(params) < 1:
+            print "Incorrect parameters"
+            return
+
+        dirname = params[0]
+        if dirname[0] != "/":
+            dirname = "%s/%s" % (self.__curdir, dirname)
+
+        perm = params[1] if len(params) > 1 else "777"
+        print self.Put(self.weburl + dirname, "mkdir", self.user, 
                         params={"permission" : perm},
-                        curl=self.__curl)
+                        curl=self.curl)
 
 
-    def cp(self, localfile, filename) :
+    def do_cp(self, data):
+        params = data.split()
+        if len(params) != 2:
+            print "Incorrect parameters"
+            return
+
+        localfile, remotefile = params
+        if remotefile[0] != "/":
+            remotefile = "%s/%s" % (self.__curdir, remotefile)
         with file(localfile) as f:
-            result = self.Put(self.weburl + filename, "cp", self.__user, 
+            result = self.Put(self.weburl + remotefile, "cp", self.user, 
                               params={"overwrite" : "true"}, 
                               data=f.read(),
-                              curl=self.__curl, 
-                              text=True,
+                              curl=self.curl, 
                               expected=(STATUS_CREATED,))
 
             if result is not None:
-                return {"status" : "OK"}
+                print {"status" : "OK"}
+            else:
+                print "Failed"
 
 
-    def append(self, localfile, filename) :
+    def do_append(self, data):
+        params = data.split()
+        if len(params) != 2:
+            print "Incorrect parameters"
+            return
+
+        localfile, remotefile = params
+        if remotefile[0] != "/":
+            remotefile = "%s/%s" % (self.__curdir, remotefile)
         with file(localfile) as f:
-            result = self.Post(self.weburl + filename, "append", self.__user,
+            result = self.Post(self.weburl + remotefile, "append", self.user,
                                data=f.read(),
-                               curl=self.__curl)
+                               curl=self.curl)
 
-            if result is not None:
-                return {"status" : "OK"}
-
-
-    def delete(self, filename) : 
-        return self.Delete(self.weburl + filename, self.__user,
-                           curl=self.__curl)
+            print {"status" : "OK"} if r is not None else "Failed"
 
 
-    def stat(self, filename):
-        r = self.Get(self.weburl + filename, "stat", self.__user,
-                     curl=self.__curl)
-        if r is not None:
-            return gen_fileinfo(result["FileStatus"])
+    def do_delete(self, filename) : 
+        if filename:
+            if filename[0] != "/":
+                filename = "/user/%s/%s" % (self.user, filename)
+            print self.Delete(self.weburl + filename, self.user, curl=self.curl)
+        else:
+            print "Missing filename"
 
 
-    def cat(self, filename):
-        return self.Get(self.weburl + filename, "cat", self.__user,
-                        curl=self.__curl,
-                        text=True)
+    def do_cat(self, filename):
+        if filename:
+            if filename[0] != "/":
+                filename = "/user/%s/%s" % (self.user, filename)
+            print self.Get(self.weburl + filename, "cat", self.user,
+                        curl=self.curl, text=True)
+        else:
+            print "Missing filename"
 
 
-    def rename(self, srcname, destname):
-        return self.Put(self.weburl + srcname, "rename", self.__user,
+    def do_rename(self, data):
+        params = data.split()
+        if len(params) != 2:
+            print "Incorrect parameters"
+            return
+
+        srcname, destname = params
+        if srcname[0] != "/":
+            srcname = "/user/%s/%s" % (self.user, srcname)
+        if destname[0] != "/":
+            destname = "/user/%s/%s" % (self.user, destname)
+        return self.Put(self.weburl + srcname, "rename", self.user,
                         params={"destination" : destname}, 
-                        curl=self.__curl)
+                        curl=self.curl)
 
 
-    def chmod(self, filename, perm="777"):
-        r = self.Put(self.weburl + filename, "chmod", self.__user,
-                     params={"permission" : perm}, curl=self.__curl)
-        if r is not None:
-            return {"status" : "OK"}
+    def do_chmod(self, data):
+        params = data.split()
+        if len(params) != 2:
+            print "Incorrect parameters"
+            return
+
+        perm, filename = params
+        if filename[0] != "/":
+            filename = "%s/%s" % (self.__curdir, filename)
+        r = self.Put(self.weburl + filename, "chmod", self.user,
+                     params={"permission" : perm}, curl=self.curl)
+
+        print {"status" : "OK"} if r is not None else "Failed"
 
 
-    def chown(self, filename, owner, group=None):
-        r = self.Put(self.weburl + filename, "chown", self.__user,
+    def do_chown(self, data):
+        params = data.split()
+        if len(params) != 2:
+            print "Incorrect parameters"
+            return
+
+        owner, filename = params
+        if filename[0] != "/":
+            filename = "%s/%s" % (self.__curdir, filename)
+        group = None
+        if owner.count(":") == 1:
+            owner, group = owner.split(":")
+
+        r = self.Put(self.weburl + filename, "chown", self.user,
                      params={"owner" : owner, "group" : group}, 
-                     curl=self.__curl)
-        if r is not None:
-            return {"status" : "OK"}
+                     curl=self.curl)
+
+        print {"status" : "OK"} if r is not None else "Failed"
 
 
 #
 # ---- main ----
 if __name__ == "__main__":
-    import sys
-    import json
-    from optparse import OptionParser
-
-    parser = OptionParser()
-    parser.add_option("--host", default="localhost")
-    parser.add_option("--curl", action="store_true", default=False)
-    parser.add_option("-u", "--user", default="caiche")
-
-    opts, args = parser.parse_args()
-
-    if __debug__: print opts, args
-
-    if len(args) < 1:
-        print "Missing arguments, supported methods are", WebHdfs.operations
-        sys.exit(1)
-
-    hdfs = WebHdfs(opts.host, opts.user, opts.curl)
-
-    method = args[0]
-    if not method in hdfs:
-        print "Unsupported method '%s'" % method
-        sys.exit(1)
-
-    try:
-        fun = getattr(hdfs, method)
-        result = fun(*args[1:])
-        if isinstance(result, (dict, list)):
-            print json.dumps(result, indent=4)
-        else:
-            print result
-
-    except AttributeError as e:
-        print e
-
-
+    print "WebHdfs"
+#    import sys
+#    import json
+#    from optparse import OptionParser
+#
+#    parser = OptionParser()
+#    parser.add_option("--host", default="localhost")
+#    parser.add_option("--curl", action="store_true", default=False)
+#    parser.add_option("-u", "--user", default="caiche")
+#
+#    opts, args = parser.parse_args()
+#
+#    if __debug__: print opts, args
+#
+#    if len(args) < 1:
+#        print "Missing arguments, supported methods are", WebHdfs.operations
+#        sys.exit(1)
+#
+#    hdfs = WebHdfs(opts.host, opts.user, opts.curl)
+#
+#    method = args[0]
+#    if not method in hdfs:
+#        print "Unsupported method '%s'" % method
+#        sys.exit(1)
+#
+#    try:
+#        fun = getattr(hdfs, method)
+#        result = fun(*args[1:])
+#        if isinstance(result, (dict, list)):
+#            print json.dumps(result, indent=4)
+#        else:
+#            print result
+#
+#    except AttributeError as e:
+#        print e
+#
+#
